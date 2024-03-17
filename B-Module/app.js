c6977e81a08c5a2b23240344d1aa2661d8c99e63
sxxngwxn <sxxngw0n@gmail.com>
2024-03-17 20:41:07 +0900

    js chart init funtion

--- B-Module/app.js
+++ B-Module/app.js
@@ -1,6 +1,30 @@
+// support by [parkmini]
+
 // visitors.json 가져오기
 async function visitorsData() {
     return fetch("/선수제공파일/B_Module/visitors.json")
     .then(object => {return object.json()})
     .then(data => {return data["data"]})
+}
+
+async function initChartData() {
+    const visitorsData = await visitorsData();
+
+    visitorsData.forEach((league) => {
+
+        $("#leagueSelect").append(new Option(league.name, league.name));
+    });
+
+    const days = ["월", "화", "수", "목", "금", "토", "일"]
+    days.forEach((day) => {
+        $("#daySelect").append(new Option(day, day));
+    });
+
+    $("#leagueSelect", "#daySelect", "#directionSelect")
+        .change(() => {
+            const league = $("#leagueSelect").val
+            const day = $("#daySelect").val
+            const direction = $("#directionSelect").val
+        })
+        .change();
 }

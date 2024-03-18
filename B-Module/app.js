2f14a0a97d9f4f406a29b67b8b36464de2605fcc
sxxngwxn <sxxngw0n@gmail.com>
2024-03-18 19:05:27 +0900

    js 코드 이해, updateChart 함수 틀 제작

--- B-Module/app.js
+++ B-Module/app.js
@@ -11,20 +11,26 @@ async function initChartData() {
     const visitorsData = await visitorsData();
 
     visitorsData.forEach((league) => {
-
         $("#leagueSelect").append(new Option(league.name, league.name));
     });
+    // visitorData 수 만큼 #leagueSelect에 option을 추가
 
     const days = ["월", "화", "수", "목", "금", "토", "일"]
     days.forEach((day) => {
         $("#daySelect").append(new Option(day, day));
     });
+    // days수 만큼 #daySelect에 option을 추가
 
     $("#leagueSelect", "#daySelect", "#directionSelect")
         .change(() => {
             const league = $("#leagueSelect").val
             const day = $("#daySelect").val
             const direction = $("#directionSelect").val
+            updateChart(visitorsData, league, day, direction)
         })
         .change();
+}
+
+function updateChart(visitorData, league, day, direction) {
+
 }

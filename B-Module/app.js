252aedcedd344548d34466863a425119b3750626
sxxngwxn <sxxngw0n@gmail.com>
2024-03-21 21:10:05 +0900

    visitor chart implement

--- B-Module/app.js
+++ B-Module/app.js
@@ -37,8 +37,23 @@ function updateChart(visitorData, leagueData, dayData, directionData) {
     const day = league.visitors.find((d) => d.day === dayData)
     console.log(day);
     const dayVisitor = day.visitor
+    const direction  = directionData
     $("#visitorTable").empty()
+    $("#visitorChart").empty()
+    $("#visitorTable").append(`<thead><th>시간대</th><th>방문자 수</th></thead>`)
     $.each(dayVisitor, (time, person) => {
         $("#visitorTable").append(`<tr><td>${time}</td><td>${person}</td></tr>`)
+        const chartVal = (person > 500) ? 500 : person
+        if (direction == "horizontal") {
+            $('#visitorChart').removeClass('visitorChartV');
+            $("#visitorChart").addClass('visitorChartH')
+            $("#visitorChart").append(`<div style="width: ${chartVal}px; height: 40px; background-color: #0d6efd; margin: 10px 0;"><p>${person}</p></div>`)
+        } else {
+            $('#visitorChart').removeClass('visitorChartH');
+            $("#visitorChart").addClass('visitorChartV')
+            $("#visitorChart").append(`<div style="width: 40px; height: ${chartVal}px; background-color: #0d6efd; display: flex; justify-content: center;"><p>${person}</p></div>`)
+        }
     })
 }
+
+initChartData()
